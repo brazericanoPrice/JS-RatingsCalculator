9f27a8742921fdb2d0bb6e2e0d55b0ec40395bf9
Richard Price <richard.price@wgu.edu>
2021-07-28 19:14:15 -0400

    commit

--- js/ratings.js
+++ js/ratings.js
@@ -11,5 +11,12 @@ function collect_ratings(){
     elements.forEach( element => {
         rating = parseInt(element.id.replace('star',''));
         ratings.count += parseInt(element.value);
+        ratings.sum += rating * parseInt(element.value);
     })
+
+    if (ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
+    }
+
+    return ratings;
 }
